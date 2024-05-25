d2185d53cccffd09509518ea003b337c45f8eac9
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-05-25 23:32:48 +0000

    [Users] - Creación modelo Users - Creación endopint get all Users - Creación endpoint get single user - Creación endpoint crear new user

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Traer todos los usuarios
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    all_users = Users.query.all()
+    users_serialized = []
+    for user in all_users:
+        users_serialized.append(user.serialize())
+    print(users_serialized)
+    return jsonify({"data": users_serialized}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Traer sólo un usuario
+@app.route('/users/<int:id>', methods=['GET'])
+def get_single_user(id):
+    single_user = Users.query.get(id)
+    if single_user is None:
+        return jsonify({"msg": "User with id: {}, not found".format(id)}), 400
+    return jsonify({"data": single_user.serialize()}), 200
 
-    return jsonify(response_body), 200
+# Crear nuevo usuario
+@app.route('/user', methods=['POST'])
+def new_user():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "You should send info in body"}), 400
+    if "name" not in body:
+        return jsonify({"msg": "Name is needed"}), 400
+    if "last_name" not in body:
+        return jsonify({"msg": "Last name is needed"}), 400
+    if "email" not in body:
+        return jsonify({"msg": "email is needed"}), 400
+    if "password" not in body:
+        return jsonify({"msg": "Password is needed"}), 400
+    
+    new_user = Users()
+    new_user.id = body.get("id", Users.generateId())
+    new_user.name = body["name"]
+    new_user.last_name = body["last_name"]
+    new_user.email = body["email"]
+    new_user.password = body["password"]
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"data": new_user.serialize()}), 201
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+last_id_users = 0
+last_id_people = 0
+last_id_starships = 0
+last_id_planets = 0
+
+class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    
+    def generateId():
+        global last_id_users
+        last_id_users += 1
+        return last_id_users
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return f"User name: {self.name}"
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name,
+            "email": self.email
         }
